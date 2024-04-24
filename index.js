81533b1e6d3aea0f9f2d5dd1a403caf2b8ebd46a
SohamChhajed <sohamchhajed007@gmail.com>
2024-04-24 21:24:18 +0530

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+// ---
+var loader = document.getElementById("loader");
+window.addEventListener("load", function () {
+	loader.style.display = "none";
+	this.document.body.removeChild(loader);
+});
+
+const hamMenuBtn = document.querySelector(".header__main-ham-menu-cont");
+const smallMenu = document.querySelector(".header__sm-menu");
+const headerHamMenuBtn = document.querySelector(".header__main-ham-menu");
+const headerHamMenuCloseBtn = document.querySelector(
+	".header__main-ham-menu-close"
+);
+const headerSmallMenuLinks = document.querySelectorAll(".header__sm-menu-link");
+
+hamMenuBtn.addEventListener("click", () => {
+	if (smallMenu.classList.contains("header__sm-menu--active")) {
+		smallMenu.classList.remove("header__sm-menu--active");
+	} else {
+		smallMenu.classList.add("header__sm-menu--active");
+	}
+	if (headerHamMenuBtn.classList.contains("d-none")) {
+		headerHamMenuBtn.classList.remove("d-none");
+		headerHamMenuCloseBtn.classList.add("d-none");
+	} else {
+		headerHamMenuBtn.classList.add("d-none");
+		headerHamMenuCloseBtn.classList.remove("d-none");
+	}
+});
+
+for (let i = 0; i < headerSmallMenuLinks.length; i++) {
+	headerSmallMenuLinks[i].addEventListener("click", () => {
+		smallMenu.classList.remove("header__sm-menu--active");
+		headerHamMenuBtn.classList.remove("d-none");
+		headerHamMenuCloseBtn.classList.add("d-none");
+	});
+}
+
+const headerLogoConatiner = document.querySelector(".header__logo-container");
+const homeButton = document.querySelector(".home_button")
+
+homeButton.addEventListener("click",()=>{
+	location.href = "index.html"
+});
+
+headerLogoConatiner.addEventListener("click", () => {
+	console.log("loggg")
+	location.href = "index.html";
+});
+
+function reloadPage() {
+    location.reload();
+}
